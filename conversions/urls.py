ec6c6437c1219183d0eb58163a2e19a8f9542f83
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-15 00:21:39 -0700

    edited routes

--- conversions/urls.py
+++ conversions/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('/conversion/', views.ConversionListCreate.as_view() ),
-    url(r'/upload/(?P<filename>[^/]+)$', views.ConversionUploadView.as_view()),
-    path('/media/', views.ConversionHTMLfile.as_view() ),
+    path('conversion', views.ConversionListCreate.as_view() ),
+    # url(r'/upload/(?P<filename>[^/]+)$', views.ConversionUploadView.as_view()),
+    url(r'upload', views.ConversionUploadAndSave.as_view()),
+    path('media', views.ConversionHTMLfile.as_view() ),
 ]

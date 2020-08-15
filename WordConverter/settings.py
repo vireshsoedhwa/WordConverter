0aef39ccf3fcc1b41934ed383dd5f7f2b0df6dcc
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-14 19:05:39 -0700

    added installed apps and media path

--- WordConverter/settings.py
+++ WordConverter/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'conversions.apps.ConversionsConfig',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/code/DATA/'
+
+MEDIA_URL = '/media/'

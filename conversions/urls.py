480ac11f52c506cf4705aa927d6de25b3fb8977e
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-14 19:02:38 -0700

    updated url patterns

---
+++ conversions/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    path('/conversion/', views.ConversionListCreate.as_view() ),
+    url(r'/upload/(?P<filename>[^/]+)$', views.ConversionUploadView.as_view())
+]

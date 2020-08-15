0e69de24fed954479b3a5d3cef3db90300524416
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-14 21:33:15 -0700

    added path for html viewing

--- conversions/urls.py
+++ conversions/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('/conversion/', views.ConversionListCreate.as_view() ),
-    url(r'/upload/(?P<filename>[^/]+)$', views.ConversionUploadView.as_view())
+    url(r'/upload/(?P<filename>[^/]+)$', views.ConversionUploadView.as_view()),
+    path('/media/', views.ConversionHTMLfile.as_view() ),
 ]

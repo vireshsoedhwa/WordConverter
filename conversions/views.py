e764ef243e4b5327ee6b676f0a0638625dceb477
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-15 00:22:33 -0700

    added file upload parser

--- conversions/views.py
+++ conversions/views.py
@@ -3,7 +3,7 @@ from .serializers import ConversionSerializer
 from rest_framework import generics
 
 from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser
 
 from rest_framework.response import Response
 
@@ -21,6 +21,7 @@ from django.core.files import File
 
 from rest_framework.decorators import api_view
 from rest_framework.renderers import StaticHTMLRenderer
+
 # Create your views here.
 
 class ConversionListCreate(generics.ListCreateAPIView):
@@ -32,24 +33,20 @@ class ConversionListCreate(generics.ListCreateAPIView):
 class ConversionHTMLfile(generics.ListCreateAPIView):
     queryset = Conversion.objects.all()
     serializer_class = ConversionSerializer
-
     
-    def get(self, request):
+    renderer_classes = [StaticHTMLRenderer]
 
+    def get(self, request, format=None):
         selected = Conversion.objects.get(pk=request.query_params['doc'])
-
-        logger.error(selected.convertedfile)
-
         with open(selected.convertedfile.path, 'r') as file:
-            data = file.read()
-
-        # logger.error(data)
-
+            data = file.read()        
         return Response(data)
 
 
+
+
 class ConversionUploadView(APIView):
-    parser_classes = [FileUploadParser]
+    parser_classes = [MultiPartParser]
 
     def put(self, request, filename, format=None):
         file_obj = request.data['file']
@@ -57,30 +54,61 @@ class ConversionUploadView(APIView):
         # do some stuff with uploaded file
         # ...
 
+        logger.error(file_obj)
+
+        # if not os.path.exists('/code/DATA/'):
+        #     os.makedirs('/code/DATA/')
+
+        # newfile = Conversion.objects.create(name=file_obj.name)
+
+        # newfile.inputfile=file_obj
+        # newfile.save()
+
+
+        # output = pypandoc.convert_file("/code/DATA/input/" + str(newfile.id) + "/" + newfile.name ,
+        #                        to='html5',
+        #                        extra_args=['--extract-media=/code/DATA/converted/' + str(newfile.id)],
+        #                        format='docx')
+
+        # output, errors = tidy_document(output)
+        # with open("/code/DATA/converted/" + str(newfile.id) + "/index.html", 'w') as f:
+        #     f.write(output)
+        
+
+        # f = open("/code/DATA/converted/" + str(newfile.id) + "/index.html")
+        # convertedfile = File(f)
+     
+        # newfile.convertedfile = convertedfile
+        # logger.error(newfile.convertedfile.url)
+
+        # newfile.save()
+
+        return Response(status=204)
+
+
+class ConversionUploadAndSave(APIView):
+    parser_classes = [MultiPartParser]
+
+    def post(self, request, format=None):
+        file_obj = request.data['file']
+
+        logger.error(request.method)
+        logger.error(request.FILES['file'])
+        logger.error(file_obj)
+
         if not os.path.exists('/code/DATA/'):
             os.makedirs('/code/DATA/')
 
         newfile = Conversion.objects.create(name=file_obj.name)
 
         newfile.inputfile=file_obj
-        # newfile.convertedfile = file_obj
         newfile.save()
 
-        # logger.error(newfile.id)
-        # logger.error(newfile.inputfile.name)
 
         output = pypandoc.convert_file("/code/DATA/input/" + str(newfile.id) + "/" + newfile.name ,
                                to='html5',
                                extra_args=['--extract-media=/code/DATA/converted/' + str(newfile.id)],
                                format='docx')
-
-        # if not os.path.exists('/code/DATA/temp/'):
-        #     os.makedirs('/code/DATA/temp/')
-
-        # directory = str(newfile.id)
-        # parent_dir = "/code/DATA/temp/"
-        # path = os.path.join(parent_dir, directory) 
-        # os.mkdir(path) 
 
         output, errors = tidy_document(output)
         with open("/code/DATA/converted/" + str(newfile.id) + "/index.html", 'w') as f:

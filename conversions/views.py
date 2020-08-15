7f576c9ca8e3db774debd5ebe6dff014a5316967
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-15 16:38:49 -0700

    added csrf exemption and cleanup

--- conversions/views.py
+++ conversions/views.py
@@ -27,10 +27,14 @@ from django.http import HttpResponse
 from django.http import FileResponse
 
 from django.shortcuts import render
+
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 class ConversionListCreate(generics.ListCreateAPIView):
-    queryset = Conversion.objects.all().order_by('-id')[:5]
+    queryset = Conversion.objects.all().order_by('-id')[:4]
     serializer_class = ConversionSerializer
 
 
@@ -40,7 +44,7 @@ class ConversionHTMLfile(generics.ListCreateAPIView):
     serializer_class = ConversionSerializer
     
     renderer_classes = [StaticHTMLRenderer]
-
+    @xframe_options_exempt
     def get(self, request, format=None):
         selected = Conversion.objects.get(pk=request.query_params['doc'])
         with open(selected.convertedfile.path, 'r') as file:
@@ -50,63 +54,23 @@ class ConversionHTMLfile(generics.ListCreateAPIView):
 
 
 def ConversionImage(request, id, targetfile):
-    logger.error(id)
-    logger.error(targetfile)
+    # logger.error(id)
+    # logger.error(targetfile)
     response = FileResponse(open("/code/DATA/converted/" + str(id) + "/media/" + targetfile, 'rb'))
     return response
-
-
-
-class ConversionUploadView(APIView):
-    parser_classes = [MultiPartParser]
-
-    def put(self, request, filename, format=None):
-        file_obj = request.data['file']
-        # ...
-        # do some stuff with uploaded file
-        # ...
-
-        logger.error(file_obj)
-
-        # if not os.path.exists('/code/DATA/'):
-        #     os.makedirs('/code/DATA/')
-
-        # newfile = Conversion.objects.create(name=file_obj.name)
-
-        # newfile.inputfile=file_obj
-        # newfile.save()
-
-
-        # output = pypandoc.convert_file("/code/DATA/input/" + str(newfile.id) + "/" + newfile.name ,
-        #                        to='html5',
-        #                        extra_args=['--extract-media=/code/DATA/converted/' + str(newfile.id)],
-        #                        format='docx')
-
-        # output, errors = tidy_document(output)
-        # with open("/code/DATA/converted/" + str(newfile.id) + "/index.html", 'w') as f:
-        #     f.write(output)
-        
-
-        # f = open("/code/DATA/converted/" + str(newfile.id) + "/index.html")
-        # convertedfile = File(f)
-     
-        # newfile.convertedfile = convertedfile
-        # logger.error(newfile.convertedfile.url)
-
-        # newfile.save()
-
-        return Response(status=204)
-
 
 class ConversionUploadAndSave(APIView):
     parser_classes = [MultiPartParser]
 
+    @csrf_exempt
     def post(self, request, format=None):
         file_obj = request.data['file']
 
-        logger.error(request.method)
-        logger.error(request.FILES['file'])
-        logger.error(file_obj)
+        # logger.error(request.method)
+        # logger.error(request.FILES['file'])
+        # logger.error(file_obj)
+
+
 
         if not os.path.exists('/code/DATA/'):
             os.makedirs('/code/DATA/')
@@ -131,7 +95,6 @@ class ConversionUploadAndSave(APIView):
         convertedfile = File(f)
      
         newfile.convertedfile = convertedfile
-        logger.error(newfile.convertedfile.url)
 
         newfile.save()
 

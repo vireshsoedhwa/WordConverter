e9493cd026e079092bbaad63ba30c50a6790c1ca
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2020-08-14 19:02:06 -0700

    created conversion model

---
+++ conversions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.files.storage import FileSystemStorage
+# Create your models here.
+
+# fs = FileSystemStorage(location='uploads/')
+
+def file_directory_path(instance, filename):
+    return 'input/{0}/{1}'.format(instance.id, filename)
+
+
+class Conversion(models.Model):
+    name = models.CharField(max_length=100)
+    inputfile = models.FileField(upload_to=file_directory_path)
+    convertedfile = models.FileField(upload_to='converted/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+

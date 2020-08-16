b57c17bb0d5fbbbe659d2f623115ee62b0689b42
Viresh Soedhwa <vsoedhwa@bcit.ca>
2020-08-15 20:42:33 -0700

    removed generation of frontend assets build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,18 @@
+echo "-------------------------------------------------------------------------------------------\n"
+
+# >&2 echo "Installing frontend dependencies..."
+# (cd frontend; npm install)
+# >&2 echo "Dependencies installed"
+# echo "-------------------------------------------------------------------------------------------\n"
+# >&2 echo "Compiling Frontend Assets..."
+# (cd frontend; npm run build)
+# >&2 echo "Frontend Assets is ready."
+# echo "-------------------------------------------------------------------------------------------\n"
+
 
 >&2 echo "Run Database migrations"
 python /code/manage.py migrate
 
-echo "-------------------------------------------------------------------------------------------\n"
-
->&2 echo "Installing frontend dependencies..."
-(cd frontend; npm install)
->&2 echo "Dependencies installed"
-echo "-------------------------------------------------------------------------------------------\n"
->&2 echo "Compiling Frontend Assets..."
-(cd frontend; npm run build)
->&2 echo "Frontend Assets is ready."
 echo "-------------------------------------------------------------------------------------------\n"
 
 #Start NGINX
